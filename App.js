59e2e43ab647d1d78aab89b4fd7e31017f8ef9da
fikrixfajar <fikrifajar.18@gmail.com>
2022-07-13 20:03:35 +0700

    Menambahkan keyboardAvoidingView

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Task  from "./components/Task";
+import { StatusBar } from "expo-status-bar";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-     
-    {/* Today's Task*/}
-    <View style={styles.taskWrapper}>
-      <Text style={styles.sectionTitle}>Today's Task</Text>
-      <View style={styles.items}>
-        {/* This is the task will go */}
-       <Task text={"Task Pertama Bray"} />
-       <Task text={"Ini Coba task kedua"} />
-       <Task text={"Mengerjakan skripsi"} />
+      {/* Today's Task*/}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+        <View style={styles.items}>
+          {/* This is the task will go */}
+          <Task text={"Task Pertama Bray"} />
+          <Task text={"Ini Coba task kedua"} />
+          <Task text={"Mengerjakan skripsi"} />
+        </View>
       </View>
 
-    </View>
-
-
+      {/* Write a task section */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.WriteTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -26,15 +43,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#ebeaed",
   },
   taskWrapper: {
     paddingTop: 80,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
-  items: {}
+  items: {
+    marginTop: 30,
+  },
+
+  WriteTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {},
+  addWrapper: {},
+  addText: {},
 });
